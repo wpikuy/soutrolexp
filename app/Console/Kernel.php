d35964440a33dc6fbd6febae8750aa9ffa5af057
Roslairy <wpikuy@qq.com>
2015-04-13 22:43:34 +0800

    Ver.0.3.7
    bug fixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Dataset;
 
 class Kernel extends ConsoleKernel {
 
@@ -24,6 +25,18 @@ class Kernel extends ConsoleKernel {
 	{
 		$schedule->command('inspire')
 				 ->hourly();
+		$schedule->call(function (){
+			$keys = [
+					'machine_1_source_1_voltage',
+					'machine_1_source_1_current',
+					'machine_1_source_1_state',
+			];
+			foreach ($keys as $key){
+				$dataSet = Dataset::where('key', '=', $key)->firstOrFail();
+				$dataSet->value = mt_rand(0, 1000);
+				$dataSet->save();
+			}
+		})->cron('* * * * *');
 	}
 
 }
